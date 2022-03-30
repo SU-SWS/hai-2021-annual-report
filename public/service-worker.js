91cd9111bef91e41abf4f0af621ec8d2fa08623c
joegl <joegl@users.noreply.github.com>
2022-03-30 12:35:58 -0500

    Added service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+// Unregister and destroy the previous 2020 site service-worker.js, which is
+// hard caching the old 2020 site for those who had loaded it.
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => {
+    self.registration.unregister();
+});
+
+// A second method to attempt to destroy the serviceworker.
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister()
+      document.location.reload()
+    }
+});
